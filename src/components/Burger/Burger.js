ef3176605d0551d9ca9f48290db93b5931ed0005
giorgi janjulia <giorgi.janjulia@gtu.ge>
2018-05-28 00:42:17 +0400

    first commit

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+	// Object.keys(props.ingredients) transform object array
+	let transformedIngredients = Object.keys(props.ingredients)
+		.map(igKey => {
+			return [...Array(props.ingredients[igKey])].map((_, i) => {
+				return <BurgerIngredient key={igKey + i} type={igKey} />;
+			});
+		})
+		.reduce((arr, el) => {
+			// merge all arrays
+			return arr.concat(el)
+		}, []);
+
+	if (transformedIngredients.length === 0) {
+		transformedIngredients = <p>Please start adding ingredient</p>
+	}
+
+	return (
+		<div className={classes.burger}>
+			<BurgerIngredient type="bread-top" />
+			{transformedIngredients}
+			<BurgerIngredient type="bread-bottom" />
+		</div>
+	);
+};
+
+export default burger;
